7eb8fa7b4b2fae4b210b89c34286df8fe0eab393
jai <jaiakash2907@gmail.com>
2024-07-29 17:53:20 +0530

    idk wht the hell is going on

--- Exp-16.c
+++ Exp-16.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
-int isPalindrome(char *ptr);
-
-int main() {
-    char str[100];
-    printf("Enter a string: ");
-    scanf("%s", str);
-    if (isPalindrome(str))
-        printf("Palindrome\n");
-    else
-        printf("Not a Palindrome\n");
-    return 0;
+// Function to reverse a sentence using recursion
+void reverseSentence() {
+    char ch;
+    scanf("%c", &ch);
+    
+    if (ch != '\n') {
+        reverseSentence();
+        printf("%c", ch);
+    }
 }
 
-int isPalindrome(char *ptr) {
-    char *start = ptr;
-    char *end = ptr + strlen(ptr) - 1;
-    while (start < end) {
-        if (*start != *end)
-            return 0;
-        start++;
-        end--;
-    }
-    return 1; 
+int main() {
+    printf("Enter a sentence: ");
+    reverseSentence();
+    printf("\n");
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,14 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    struct stud{
-        int id;
-        char name[10];
-    }students[10];
-    int i;
-    for(i=0;i<scanf("%d");i++){
-        printf("Enter Id Name:");
-        scanf("%d %s",&students[i].id,students[i].name);
-    }
+
+int main() {
+   
+    printf("helo");
     return 0;
 }
